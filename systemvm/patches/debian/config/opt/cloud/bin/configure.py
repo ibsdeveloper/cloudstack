a8f45bee0f29c382f339f5fc23d7acba872f1ccf
root <root@localhost.localdomain>
2016-08-09 11:18:53 +0530

    applied 1472 applied

--- systemvm/patches/debian/config/opt/cloud/bin/configure.py
+++ systemvm/patches/debian/config/opt/cloud/bin/configure.py
@@ -75,30 +75,6 @@ class CsPassword(CsDataBag):
                 logging.debug("Update password server result ==> %s" % result)
 
 
-<<<<<<< HEAD
-class CsStaticRoutes(CsDataBag):
-    
-    def process(self):
-        logging.debug("Processing CsStaticRoutes file ==> %s" % self.dbag)
-        for item in self.dbag:
-            if item == "id":
-                continue
-            self.__update(self.dbag[item])
-
-    def __update(self, route):
-        if route['revoke']:
-            command = "route del -net %s gw %s" % (route['network'], route['gateway'])
-            result = CsHelper.execute(command)
-        else:
-            command = "ip route show | grep %s | awk '{print $1, $3}'" % route['network']
-            result = CsHelper.execute(command)
-            if not result:
-                route_command = "route add -net %s gw %s" % (route['network'], route['gateway'])
-                result = CsHelper.execute(route_command)
-
-
-=======
->>>>>>> b9feb39... apply static routes on change to master state
 class CsAcl(CsDataBag):
     """
         Deal with Network acls
@@ -943,26 +919,6 @@ def main(argv):
         metadata = CsVmMetadata('vmdata', config)
         metadata.process()
 
-    # Always run both CsAcl().process() methods
-    # They fill the base rules in config.fw[]
-    acls = CsAcl('networkacl', config)
-    acls.process()
-
-    acls = CsAcl('firewallrules', config)
-    acls.process()
-
-    fwd = CsForwardingRules("forwardingrules", config)
-    fwd.process()
-
-    vpns = CsSite2SiteVpn("site2sitevpn", config)
-    vpns.process()
-
-    rvpn = CsRemoteAccessVpn("remoteaccessvpn", config)
-    rvpn.process()
-
-    lb = CsLoadBalancer("loadbalancer", config)
-    lb.process()
-
     if process_file in ["cmd_line.json", "network_acl.json"]:
         logging.debug("Configuring networkacl")
         iptables_change = True
@@ -1004,9 +960,33 @@ def main(argv):
 
     # If iptable rules have changed, apply them.
     if iptables_change:
+        acls = CsAcl('networkacl', config)
+        acls.process()
+
+        acls = CsAcl('firewallrules', config)
+        acls.process()
+
+        fwd = CsForwardingRules("forwardingrules", config)
+        fwd.process()
+
+        vpns = CsSite2SiteVpn("site2sitevpn", config)
+        vpns.process()
+
+        rvpn = CsRemoteAccessVpn("remoteaccessvpn", config)
+        rvpn.process()
+
+        lb = CsLoadBalancer("loadbalancer", config)
+        lb.process()
+
         logging.debug("Configuring iptables rules")
         nf = CsNetfilters()
         nf.compare(config.get_fw())
+
+        logging.debug("Configuring iptables rules done ...saving rules")
+
+        # Save iptables configuration - will be loaded on reboot by the iptables-restore that is configured on /etc/rc.local
+        CsHelper.save_iptables("iptables-save", "/etc/iptables/router_rules.v4")
+        CsHelper.save_iptables("ip6tables-save", "/etc/iptables/router_rules.v6")
 
     red = CsRedundant(config)
     red.set()
@@ -1015,13 +995,6 @@ def main(argv):
         logging.debug("Configuring static routes")
         static_routes = CsStaticRoutes("staticroutes", config)
         static_routes.process()
-
-    if iptables_change:
-        logging.debug("Configuring iptables rules done ...saving rules")
-
-        # Save iptables configuration - will be loaded on reboot by the iptables-restore that is configured on /etc/rc.local
-        CsHelper.save_iptables("iptables-save", "/etc/iptables/router_rules.v4")
-        CsHelper.save_iptables("ip6tables-save", "/etc/iptables/router_rules.v6")
 
 if __name__ == "__main__":
     main(sys.argv)
